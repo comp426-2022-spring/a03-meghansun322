09ad138bdf50c0a43f09af9739ff2980fcd35e49
Meghan Sun <meghansun322@gmail.com>
2022-02-15 16:43:17 -0500

    add app listener

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require("express");
 const app = express();
 
 var port = 5555;
+
+const server = app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
+
+// Put listen at the end
