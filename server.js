4aa762a3c8273359be7c3303572a51e9a6947e36
Meghan Sun <meghansun322@gmail.com>
2022-02-24 14:52:57 -0500

    completed assignent 03

--- server.js
+++ server.js
@@ -15,6 +15,54 @@ function coinFlip() {
   return result;
 }
 
+function coinFlips(flips) {
+  let flip_list = [];
+  let i = 0;
+  while (i < flips) {
+    flip_list.push(coinFlip());
+    i++;
+  }
+  return flip_list;
+}
+
+function countFlips(array) {
+  var counter;
+  var heads = 0;
+  var tails = 0;
+  var i = 0;
+  while (i < array.length) {
+    if (array[i] === "tails") {
+      tails = tails + 1;
+    } else {
+      heads = heads + 1;
+    }
+    i++;
+  }
+  if (heads == 0) {
+    counter = { tails };
+  } else if (tails == 0) {
+    counter = { heads };
+  } else {
+    counter = { tails, heads };
+  }
+  return counter;
+}
+
+function flipACoin(call) {
+  var return_statement = {
+    call,
+    flip: coinFlip(),
+    result: "",
+  };
+
+  if (return_statement.call === return_statement.flip) {
+    return_statement.result = "win";
+  } else {
+    return_statement.result = "lose";
+  }
+  return return_statement;
+}
+
 const server = app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
@@ -26,14 +74,35 @@ app.get("/app", (req, res) => {
 
 app.get("/app/echo/:number", (req, res) => {
   res.status(200).json({ message: req.params.number });
+  res.type("text/plain");
 });
 // Put listen at the end
 app.get("/app/flip", (req, res) => {
   var flip = coinFlip();
   res.status(200).json({ flip: flip });
+  res.type("text/plain");
+});
+
+app.get("/app/flips/:number", (req, res) => {
+  var flip = coinFlips(req.params.number);
+  var summary = countFlips(flip);
+  res.status(200).json({ raw: flip, summary: summary });
+  res.type("text/plain");
+});
+
+app.get("/app/flip/call/heads", (req, res) => {
+  var result = flipACoin("heads");
+  res.status(200).json({ result: result });
+  res.type("text/plain");
+});
+
+app.get("/app/flip/call/tails", (req, res) => {
+  var result = flipACoin("tails");
+  res.status(200).json({ result: result });
+  res.type("text/plain");
 });
 
 app.use(function (req, res) {
-  res.status(404).end("Endpoint does not exist");
+  res.status(404).end("404 NOT FOUND");
   res.type("text/plain");
 });
